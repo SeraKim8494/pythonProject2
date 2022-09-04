32101445ec07524f548c81dc6475b58780306664
serakim8494 <serakim00@gmail.com>
2022-09-04 16:15:37 +0900

    ok

---
+++ test3.py
@@ -0,0 +1,14 @@
+abc = int(input())
+efg = input()
+
+num3 =((abc) * int(efg[2]))
+print(num3)
+
+num4 = ((abc) * int(efg[1]))
+print(num4)
+
+num5 = ((abc) * int(efg[0]))
+print(num5)
+
+print(num3 + int(num4 * 10) + int(num5 * 100))
+
