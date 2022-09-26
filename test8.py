d0e3d96f02e6e65c03ec55792d2d193bca43520f
serakim8494 <serakim00@gmail.com>
2022-09-26 11:31:42 +0900

    ok

--- test8.py
+++ test8.py
@@ -1,5 +1,13 @@
-H, M = map(int, input().split)
+a = int(input("시작 수를 입력해주세요: "))
+b = int(input("끝 수를 입력해주세요: "))
 
-if M > 45:
-    print(H, M-45)
+for i in range(a, b + 1):
+
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        print(i, end =" ")
+
+
 
