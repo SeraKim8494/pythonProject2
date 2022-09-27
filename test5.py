77cfe45d954ddf683909cd93f21d63a210a83909
serakim8494 <serakim00@gmail.com>
2022-09-27 12:17:35 +0900

    ok

--- test5.py
+++ test5.py
@@ -1,16 +1,6 @@
-score = int(input())
+file_names = ["file1.py", "file2.txt", "file3.pptx", "file3.doc"]
 
-if score >= 90:
-    print('A')
+for i in file_names:
+    a = i.split(".")
+    print("%s => 파일명:%s, 확장자:.%s" %(i,a[0],a[1]))
 
-elif score >=80:
-    print('B')
-
-elif score >=70:
-    print('C')
-
-elif score >=60:
-    print('D')
-
-else:
-    print('F')
